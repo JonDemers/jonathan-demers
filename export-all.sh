c1f4b7d43ff5ec5913040f5fdf8d5a34a0ac8856
Jonathan Demers <jdemers@opcodesolutions.com>
2020-05-20 07:18:20 -0400

    Add resume for Senior Full Stack Developer

--- export-all.sh
+++ export-all.sh
@@ -8,6 +8,7 @@ set -e
 
 
 for position_title_normalcase in \
+  "Senior Full Stack Developer" \
   "Expert Java Developer" \
   "Manager, Application Development and Automation" \
   "Senior Java Tech Lead" \
